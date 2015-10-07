1c9c6a431b81990e6e16d486e4769f5d17791ba0
Marco Schaelle <marcoschaelle@gmail.com>
2015-10-07 21:29:47 +0200

    working emitter for iteration

--- TerrificNet.Thtml/Emit/EmitExpressionVisitor.cs
+++ TerrificNet.Thtml/Emit/EmitExpressionVisitor.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TerrificNet.Thtml.Parsing.Handlebars;
+using TerrificNet.Thtml.VDom;
 
 namespace TerrificNet.Thtml.Emit
 {
@@ -8,7 +12,7 @@ namespace TerrificNet.Thtml.Emit
         private readonly Stack<DataBinderResult> _dataBinder = new Stack<DataBinderResult>();
         private DataBinderResult Scope => _dataBinder.Peek();
 
-        public DataBinderResult Value { get; private set; }
+        private DataBinderResult Value { get; set; }
 
         public EmitExpressionVisitor(IDataBinder dataBinder)
         {
@@ -16,26 +20,70 @@ namespace TerrificNet.Thtml.Emit
             Value = Scope;
         }
 
-        public override bool BeforeVisit(IterationExpression iterationExpression)
-        {
-
-            return true;
-        }
-
-        public override void AfterVisit(IterationExpression iterationExpression)
-        {
-            _dataBinder.Push(Value.Item());
-        }
-
         public override bool BeforeVisit(MemberExpression memberExpression)
         {
-            Value = Scope.Evaluate(memberExpression.Name);
+            if (memberExpression.Name == "this")
+                Value = Scope;
+            else
+                Value = Scope.Evaluate(memberExpression.Name);
+
             return false;
         }
 
-        public void AfterStatement()
+        public void EnterScope(MustacheExpression expression)
+        {
+            var iterationExpression = expression as IterationExpression;
+            if (iterationExpression != null)
+                expression = iterationExpression.Expression;
+
+            expression.Accept(this);
+            _dataBinder.Push(Value.Item());
+        }
+
+        public IListEmitter<VTree> LeaveScope(MustacheExpression expression, IListEmitter<VTree> children)
         {
             _dataBinder.Pop();
+
+            var iterationExpression = expression as IterationExpression;
+            if (iterationExpression != null)
+            {
+                iterationExpression.Expression.Accept(this);
+                if (Value.ResultType.GetInterfaces().Contains(typeof(IEnumerable)))
+                {
+                    var evaluation = Wrap(Value.CreateEvaluation<IEnumerable>(), expression);
+                    return EmitterNode.Iterator(d => evaluation(d), children);
+                }
+
+                throw new Exception("Expect a enumerable as result");
+            }
+
+            if (Value.ResultType == typeof(string))
+            {
+                var evaluation = Wrap(Value.CreateEvaluation<string>(), expression);
+                return EmitterNode.AsList(EmitterNode.Lambda(d => new VText(evaluation(d))));
+            }
+
+            if (Value.ResultType == typeof(VText))
+            {
+                return EmitterNode.AsList(EmitterNode.Lambda(Wrap(Value.CreateEvaluation<VText>(), expression)));
+            }
+
+            throw new Exception("Expect a VText or string as result");
+        }
+
+        private static Func<IDataContext, T> Wrap<T>(Func<IDataContext, T> createEvaluation, MustacheExpression expression)
+        {
+            return c =>
+            {
+                try
+                {
+                    return createEvaluation(c);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Exception on executing expression {expression}", ex);
+                }
+            };
         }
     }
 }
--- TerrificNet.Thtml/Emit/EmitNodeVisitor.cs
+++ TerrificNet.Thtml/Emit/EmitNodeVisitor.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using TerrificNet.Thtml.Parsing;
-using TerrificNet.Thtml.Parsing.Handlebars;
 using TerrificNet.Thtml.VDom;
 
 namespace TerrificNet.Thtml.Emit
@@ -11,7 +7,9 @@ namespace TerrificNet.Thtml.Emit
     internal class EmitNodeVisitor : NodeVisitor
     {
         private readonly EmitExpressionVisitor _expressionVisitor;
-        private readonly List<IListEmitter<VTree>> _elements = new List<IListEmitter<VTree>>();
+        private readonly Stack<List<IListEmitter<VTree>>> _elements = new Stack<List<IListEmitter<VTree>>>();
+
+        private List<IListEmitter<VTree>> Scope => _elements.Peek();
 
         public EmitNodeVisitor(IDataBinder dataBinder) : this(new EmitExpressionVisitor(dataBinder))
         {
@@ -26,100 +24,58 @@ namespace TerrificNet.Thtml.Emit
 
         public override bool BeforeVisit(Element element)
         {
+            EnterScope();
             return true;
         }
 
         public override void AfterVisit(Element element)
         {
-            var children = _elements.ToList();
-            _elements.Clear();
-            var emitter = EmitterNode.Many(children);
-            _elements.Add(EmitterNode.AsList(EmitterNode.Lambda(d => new VElement(element.TagName, emitter.Execute(d)))));
+            var emitter = EmitterNode.Many(LeaveScope());
+            Scope.Add(EmitterNode.AsList(EmitterNode.Lambda(d => new VElement(element.TagName, emitter.Execute(d)))));
         }
 
         public override void Visit(TextNode textNode)
         {
-            _elements.Add(EmitterNode.AsList(EmitterNode.Lambda(d => new VText(textNode.Text))));
+            Scope.Add(EmitterNode.AsList(EmitterNode.Lambda(d => new VText(textNode.Text))));
         }
 
         public override bool BeforeVisit(Document document)
         {
+            EnterScope();
             return true;
         }
 
         public override void AfterVisit(Document document)
         {
-            var emitter = EmitterNode.Many(_elements);
+            var emitter = EmitterNode.Many(LeaveScope());
             DocumentFunc = EmitterNode.Lambda(d => new VNode(emitter.Execute(d)));
         }
 
         public override bool BeforeVisit(Statement statement)
         {
-            statement.Expression.Accept(_expressionVisitor);
+            _expressionVisitor.EnterScope(statement.Expression);
+            EnterScope();
+
             return true;
         }
 
         public override void AfterVisit(Statement statement)
         {
-            IListEmitter<VTree> listEmitter;
-            if (statement.ChildNodes.Length == 0)
-            {
-                listEmitter = EmitterNode.AsList(EmitterNode.Lambda(GetEvaluationForVText(_expressionVisitor.Value, statement.Expression)));
-            }
-            else
-            {
-                statement.Expression.Accept(_expressionVisitor);
-                _expressionVisitor.AfterStatement();
+            var childEmitter = EmitterNode.Many(LeaveScope());
+            var listEmitter = _expressionVisitor.LeaveScope(statement.Expression, childEmitter);
 
-                var children = _elements.ToList();
-                _elements.Clear();
-                var emitter = EmitterNode.Many(children);
-                var iteratorStatement = GetEvaluationForIteration(_expressionVisitor.Value, statement.Expression);
-                listEmitter = EmitterNode.Iterator(iteratorStatement, emitter);
-            }
-            _elements.Add(listEmitter);
+            Scope.Add(listEmitter);
         }
 
-        private static Func<IDataContext, IEnumerable> GetEvaluationForIteration(DataBinderResult dataBinderResult, MustacheExpression expression)
+        private void EnterScope()
         {
-            if (dataBinderResult.ResultType.GetInterfaces().Contains(typeof (IEnumerable)))
-            {
-                var evaluation = Wrap(dataBinderResult.CreateEvaluation<IEnumerable>(), expression);
-                return d => evaluation(d);
-            }
-
-            throw new Exception("Expect a enumerable as result");
+            _elements.Push(new List<IListEmitter<VTree>>());
         }
 
-        private static Func<IDataContext, VText> GetEvaluationForVText(DataBinderResult dataBinderResult, MustacheExpression expression)
+        private List<IListEmitter<VTree>> LeaveScope()
         {
-            if (dataBinderResult.ResultType == typeof (string))
-            {
-                var evaluation = Wrap(dataBinderResult.CreateEvaluation<string>(), expression);
-                return d => new VText(evaluation(d));
-            }
-
-            if (dataBinderResult.ResultType == typeof (VText))
-            {
-                return Wrap(dataBinderResult.CreateEvaluation<VText>(), expression);
-            }
-
-            throw new Exception("Expect a VText or string as result");
+            return _elements.Pop();
         }
 
-        private static Func<IDataContext, T> Wrap<T>(Func<IDataContext, T> createEvaluation, MustacheExpression expression)
-        {
-            return c =>
-            {
-                try
-                {
-                    return createEvaluation(c);
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception($"Exception on executing expression {expression}", ex);
-                }
-            };
-        }
     }
 }
