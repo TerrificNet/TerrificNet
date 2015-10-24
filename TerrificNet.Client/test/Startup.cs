fccf8a22a7268bb3a9f87a5976b9c1f9d4193ba0
Marco Schaelle <marcoschaelle@gmail.com>
2015-10-24 12:58:55 +0200

    initial mvc setup for test project

--- TerrificNet.Client/test/Startup.cs
+++ TerrificNet.Client/test/Startup.cs
@@ -1,7 +1,18 @@
-﻿namespace TerrificNet.Client.test
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace TerrificNet.Client.test
 {
     public class Startup
     {
-        private string gugus;
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder builder)
+        {
+            builder.UseMvc();
+        }
     }
 }
---
+++ TerrificNet.Client/test/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace TerrificNet.Client.test
+{
+    [Route("test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new ObjectResult(new {Name = "gugus", Text = "text"});
+        }
+    }
+}
