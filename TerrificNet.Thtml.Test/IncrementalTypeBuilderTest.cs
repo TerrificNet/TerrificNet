9065064c98599a06ca4fc6221282958a143d5454
Marco Schälle <marcoschaelle@gmail.com>
2016-10-24 12:42:24 +0200

    Basic implementation for IncrementalTypeBuilder

---
+++ TerrificNet.Thtml.Test/IncrementalTypeBuilderTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using TerrificNet.Thtml.Emit.Compiler;
+using Xunit;
+
+namespace TerrificNet.Thtml.Test
+{
+	public class IncrementalTypeBuilderTest
+	{
+		[Fact]
+		public void IncrementalTypeBuilder_AddField_TypeContainsField()
+		{
+			var underTest = new IncrementalTypeBuilder(typeof(object));
+			var resultBuilder = underTest.AddField(typeof(string));
+
+			Assert.NotNull(resultBuilder.Type);
+			var fields = resultBuilder.Type.GetTypeInfo().GetFields().ToList();
+			Assert.Equal(1, fields.Count);
+			Assert.Equal(typeof(string), fields[0].FieldType);
+		}
+
+		[Fact]
+		public void IncrementalTypeBuilder_AddField_CanBeUsedInExpression()
+		{
+			var underTest = new IncrementalTypeBuilder(typeof(object));
+			FieldInfo fieldInfo;
+			var resultBuilder = underTest.AddField(typeof(string), out fieldInfo);
+
+			var parameter = Expression.Parameter(resultBuilder.Type);
+			var expression = Expression.Field(parameter, fieldInfo);
+
+			Assert.Equal(typeof(string), expression.Type);
+		}
+
+	}
+}
---
+++ TerrificNet.Thtml/Emit/Compiler/IncrementalTypeBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace TerrificNet.Thtml.Emit.Compiler
+{
+	public class IncrementalTypeBuilder
+	{
+		public IncrementalTypeBuilder(Type type)
+		{
+			Type = type;
+		}
+
+		public IncrementalTypeBuilder AddField(Type type)
+		{
+			FieldInfo info;
+			return AddField(type, out info);
+		}
+
+		public IncrementalTypeBuilder AddField(Type type, out FieldInfo field)
+		{
+			var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("Test"), AssemblyBuilderAccess.Run);
+			var moduleBuilder = assemblyBuilder.DefineDynamicModule("_dynamic");
+			var typeBuilder = moduleBuilder.DefineType("Test", TypeAttributes.Class | TypeAttributes.Public, Type);
+			var fieldBuilder = typeBuilder.DefineField("Gugus", type, FieldAttributes.Public);
+
+			field = fieldBuilder;
+
+			return new IncrementalTypeBuilder(typeBuilder.CreateTypeInfo().AsType());
+		}
+
+		public Type Type { get; }
+	}
+}
